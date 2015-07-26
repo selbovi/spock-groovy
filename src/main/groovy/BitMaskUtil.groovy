f147f36ee84b515c259fee3c372fb70e8ba3c071
Salavat <salavatm@gmail.com>
2015-07-27 01:05:02 +0300

    cartesian api first approach

--- src/main/groovy/BitMaskUtil.groovy
+++ src/main/groovy/BitMaskUtil.groovy
@@ -3,8 +3,53 @@
  */
 class BitMaskUtil {
 
+    /**получить int в виде бинарной строки*/
     def static intAsBinaryString(int intNum) {
         Integer.toBinaryString(intNum)
     }
 
+    /**получить результат по максе*/
+    def static getValueContainedResult(int actual, int val) {
+        actual & val
+    }
+
+    /**получить булевый результат сравнения, содржится ли число в маске*/
+    def static isValueContained(int actual, int val) {
+        getValueContainedResult(actual, val) == val
+    }
+
+    /**
+     * получить список картезиана из возможных битовых масок, куда может попадать
+     * исходное значение
+     * @param arr [1, 2, 4, 6 ...]
+     * @param val
+     * @return
+     */
+    def static possibleValuesContainingCombinatios(arr, val) {
+        def result = new ObservableSet<Integer>()
+        while (arr.size > 0) {
+            arr.each {
+
+                def a = it
+                arr.each {
+                    def b = it
+
+                    if ((a | b) >= val) {
+                        /*print("val=" + intAsBinaryString(val) + " | ")
+                        print("a=$a , b=$b | ")
+                        print("a|b = " + intAsBinaryString(a | b) + " " + (a | b))
+                        print(' | ')
+                        print(isValueContained((a | b), val))
+                        println()*/
+                        if (isValueContained((a | b), val)) {
+                            result.add(a | b)
+                        }
+                    }
+                    a = a | b
+                }
+            }
+            arr = arr.subList(1, arr.size)
+        }
+        result
+    }
 }
--- src/test/groovy/BitMaskUtilSpec.groovy
+++ src/test/groovy/BitMaskUtilSpec.groovy
@@ -32,4 +32,57 @@ class BitMaskUtilSpec extends Specification {
         'FOUR'  | 4
         'EIGHT' | 8
     }
+
+    @Unroll
+    def "test flag result"() {
+        expect:
+        BitMaskUtil.getValueContainedResult(a, b) == c
+
+        where:
+        a     | b | c
+        0b011 | 2 | 2
+        0b010 | 1 | 0
+        0b110 | 4 | 4
+    }
+
+    @Unroll
+    def "test flag enabled"() {
+        expect:
+        BitMaskUtil.isValueContained(a, b) == c
+
+        where:
+        a     | b | c
+        0b011 | 2 | true
+        0b010 | 1 | false
+        0b110 | 4 | true
+    }
+
+    @Unroll
+    def "cartesian test"() {
+        expect:
+        BitMaskUtil.possibleValuesContainingCombinatios(arr, val).size() == size
+
+        where:
+        arr              | val | size
+        [2]              | 1   | 0
+        [2]              | 4   | 0
+        [2]              | 2   | 1
+        [1, 2, 4]        | 2   | 4
+        [1, 2, 4]        | 1   | 4
+        [1, 2, 4]        | 4   | 4
+        [1, 2, 4]        | 6   | 2
+        [1, 2, 4]        | 5   | 2
+        [1, 2, 4]        | 8   | 0
+        [1, 2, 4]        | 7   | 1
+        [2, 8]           | 1   | 0
+        [2, 8]           | 8   | 2
+        [2, 8]           | 2   | 2
+        [2, 8]           | 4   | 0
+        [1, 2]           | 3   | 1
+        [1, 2]           | 4   | 0
+        [1, 2, 4, 8, 16] | 32  | 0
+        [1, 2, 4, 8, 16] | 16  | 11
+        [1, 2, 4, 8, 16] | 24  | 4
+        [1, 2, 4, 8, 16] | 1   | 11
+    }
 }
